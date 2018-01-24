04b92a20e622587cb3a516122341dbd99f44ae99
Cody <codywahl@gmail.com>
2018-01-24 12:27:32 -0800

    Initial commit

---
+++ AppointmentRetriever/AppointmentRetriever/ExchangeWebServicesWrapper.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.DirectoryServices;
+using System.Linq;
+using System.Net;
+
+namespace AppointmentRetriever
+{
+    public class ExchangeWebServicesWrapper
+    {
+        private static readonly string LdapAddress = ConfigurationManager.AppSettings["LDAPAddress"];
+        private static readonly string ExchangeUserName = ConfigurationManager.AppSettings["ExchangeUserName"];
+        private static readonly string ExchangeUserPassword = ConfigurationManager.AppSettings["ExchangeUserPassword"];
+        private static readonly string ExchangeUserDomain = ConfigurationManager.AppSettings["ExchangeUserDomain"];
+        private static readonly string ExchangeUrl = ConfigurationManager.AppSettings["ExchangeURL"];
+        private static readonly string ExchangeVersion = ConfigurationManager.AppSettings["ExchangeVersion"];
+
+        private const string LdapFilter = "(&(&(&(mailNickname=*)(objectcategory=person)(objectclass=user)(msExchRecipientDisplayType=7))))";
+
+        public static List<string> GetAllRoomAddressesFromActiveDirectory()
+        {
+            var rooms = new List<string>();
+
+            var directoryEntry = new DirectoryEntry(LdapAddress);
+            var directorySearcher = new DirectorySearcher(directoryEntry)
+            {
+                Filter = LdapFilter
+            };
+
+            directorySearcher.PropertiesToLoad.Add("sn");
+            directorySearcher.PropertiesToLoad.Add("mail");
+
+            foreach (SearchResult searchResult in directorySearcher.FindAll())
+            {
+                rooms.Add(searchResult.Properties["mail"][0].ToString());
+            }
+
+            return rooms;
+        }
+
+        public static List<Appointment> GetAppointmentsForUser(string mailboxToAccess, int numberOfMonths)
+        {
+            var service = GetExchangeService();
+
+            try
+            {
+                var startDate = DateTime.Today;
+                var endDate = DateTime.Today.AddMonths(numberOfMonths);
+                var cv = new CalendarView(startDate, endDate);
+
+                var calendarFolderId = new FolderId(WellKnownFolderName.Calendar, mailboxToAccess);
+                return service.FindAppointments(calendarFolderId, cv).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine();
+                return null;
+            }
+        }
+
+        private static ExchangeService GetExchangeService(bool traceEnabled = false)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+
+            var version = Microsoft.Exchange.WebServices.Data.ExchangeVersion.Exchange2013_SP1;
+
+            if (!string.IsNullOrWhiteSpace(ExchangeVersion) && ExchangeVersion.Equals("2013"))
+            {
+                version = Microsoft.Exchange.WebServices.Data.ExchangeVersion.Exchange2013;
+            }
+
+            var service = new ExchangeService(version)
+            {
+                TraceEnabled = traceEnabled,
+                TraceEnablePrettyPrinting = traceEnabled,
+                Credentials = new NetworkCredential(ExchangeUserName, ExchangeUserPassword, ExchangeUserDomain),
+                Url = new Uri(ExchangeUrl)
+            };
+
+            if (traceEnabled)
+            {
+                service.TraceFlags = TraceFlags.All;
+            }
+
+            // Autodiscover does not work on our test exchange server
+            // Best practice is to use the auto-discover, according to MS, but its not neccessary.
+            // Calling autodiscover after setting the url in the constructor should, in theory, overwrite it, if it works. If not, oh well.
+            try
+            {
+                service.AutodiscoverUrl($"{ExchangeUserName}@{ExchangeUserDomain}");
+            }
+            catch (AutodiscoverLocalException)
+            {
+                //Autodiscover failed, use the previously set url.
+            }
+
+            return service;
+        }
+    }
+}
---
+++ AppointmentRetriever/AppointmentRetriever/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AppointmentRetriever
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"{"Room",-35}{"Appointment Count",-45}");
+            Console.WriteLine($"-----------------------------------------------------------------------{Environment.NewLine}");
+
+            var rooms = ExchangeWebServicesWrapper.GetAllRoomAddressesFromActiveDirectory();
+
+            foreach (var room in rooms)
+            {
+                try
+                {
+                    var appointments = ExchangeWebServicesWrapper.GetAppointmentsForUser(room, 2);
+
+                    if (appointments != null)
+                    {
+                        Console.WriteLine($"{room,-35}{appointments.Count,-45}");
+
+                        if (appointments.Count > 0)
+                        {
+                            Console.WriteLine($"{Environment.NewLine}\tAppointments...");
+                        }
+
+                        foreach (var appointment in appointments)
+                        {
+                            Console.WriteLine($"\t-----------------------------------------------------------------");
+                            Console.WriteLine($"\tSubject:    {appointment.Subject}");
+                            Console.WriteLine($"\tStart Time: {appointment.Start}");
+                            Console.WriteLine($"\tEnd Time:   {appointment.End}");
+                            Console.WriteLine($"\t-----------------------------------------------------------------");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex + Environment.NewLine);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
